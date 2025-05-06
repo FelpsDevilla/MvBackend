8858b71b6b77ba2619c770b0b5caff85b96b8f8a
Felipe Devilla <felipedevilla@gmail.com>
2025-05-06 13:51:10 -0300

    Adcionado suporte ao Upload de imagem

--- src/routes/AcervoRoute.ts
+++ src/routes/AcervoRoute.ts
@@ -1,11 +1,17 @@
 import { AuthMiddleware } from "@/classes/AuthMiddleware";
 import { AcervoController } from "@/controllers/AcervoController.js";
 import  express, { Router }  from "express";
+import multer from "multer"
+import fs from "fs";
 
 const acervoRouter: Router = express.Router();
+const storagePath: string = "public/data/uploads/acervo";
 const url: string = "/acervo";
 
+const upload: multer.Multer = multer({dest: storagePath});
+
 acervoRouter.post(url, AuthMiddleware.auth, AcervoController.insertItem);
+acervoRouter.post(`${url}/image`, AuthMiddleware.auth, upload.single("image"));
 acervoRouter.get(url, AcervoController.getAllItens);
 acervoRouter.get(`${url}/:id`, AuthMiddleware.auth, AcervoController.getItemById);
 acervoRouter.put(`${url}/:id`, AuthMiddleware.auth, AcervoController.updateItem);
